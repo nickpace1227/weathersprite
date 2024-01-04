21bc7f7e4b67b3074100aa8bd73a72fe34e3684b
Nick Pace <nicholasppace@gmail.com>
2024-01-04 15:31:03 -0500

    began work on API calls

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 export default function Home() {
+    const fetchFunc = () => {
+       fetch("https://api.weather.gov/gridpoints/PDT/57,165/forecast?units=us")
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+    
+
     return(
-        <div>stuff</div>
+        <button type="button" onClick={fetchFunc}>stuff</button>
     );
 }
