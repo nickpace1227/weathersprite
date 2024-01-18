1f9ec47ab41b873abad0cd6596db090935743276
Nick Pace <nicholasppace@gmail.com>
2024-01-18 14:45:29 -0500

    updated how forecast is saved on 1 day

--- src/components/OneDay/index.js
+++ src/components/OneDay/index.js
@@ -2,14 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Wrapper } from "./styles.js";
 
 export default function OneDay(props) {
-  const [currentTemp, setCurrentTemp] = useState("");
-  const [feelsLike, setFeelsLike] = useState("");
-  const [highTemp, setHighTemp] = useState("");
-  const [lowTemp, setLowTemp] = useState("");
-  const [humidity, setHumidity] = useState("");
   const [hasFetched, setHasFetched] = useState(false);
   const [units, setUnits] = useState("");
-  const forecast = [];
+  const [forecast, setForecast] = useState({});
 
   const callWeather = async () => {
     if (props.lat !== "" && props.lon !== "") {
@@ -25,12 +20,14 @@ export default function OneDay(props) {
           https://api.openweathermap.org/data/2.5/weather?lat=${props.lat}&lon=${props.lon}&appid=3629692cef6e7a55af67ced0043c6264&units=${props.units}`);
         const processedWeather = await weather.json();
         console.log(processedWeather);
-        setCurrentTemp(Math.floor(processedWeather["main"].temp));
-        setFeelsLike(Math.floor(processedWeather["main"].feels_like));
-        setHighTemp(Math.floor(processedWeather["main"].temp_max));
-        setLowTemp(Math.floor(processedWeather["main"].temp_min));
-        setHumidity(Math.floor(processedWeather["main"].humidity));
-        forecast.push(processedWeather);
+
+        setForecast({
+          currentTemp: Math.floor(processedWeather["main"].temp),
+          feelsLike: Math.floor(processedWeather["main"].feels_like),
+          highTemp: Math.floor(processedWeather["main"].temp_max),
+          lowTemp: Math.floor(processedWeather["main"].temp_min),
+          humidity: Math.floor(processedWeather["main"].humidity),
+        });
         setHasFetched(true);
       } catch (err) {
         console.log(err);
@@ -48,24 +45,18 @@ export default function OneDay(props) {
         <div className="forecast-card">
           <div className="forecast-header">Today's Forecast</div>
           <div className="forecast-value">
-            Current Temp: {currentTemp}&deg;{units}
+            Current Temp: {forecast.currentTemp}&deg;{units}
           </div>
           <div className="forecast-value">
-            Feels Like: {feelsLike}&deg;{units}
+            Feels Like: {forecast.feelsLike}&deg;{units}
           </div>
           <div className="forecast-value">
-            Today's High: {highTemp}&deg;{units}
+            Today's High: {forecast.highTemp}&deg;{units}
           </div>
-
           <div className="forecast-value">
-            Today's High: {highTemp}&deg;{units}
+            Today's Low: {forecast.lowTemp}&deg;{units}
           </div>
-
-          <div className="forecast-value">
-            Today's Low: {lowTemp}&deg;{units}
-          </div>
-
-          <div className="forecast-value">Humidity: {humidity}%</div>
+          <div className="forecast-value">Humidity: {forecast.humidity}%</div>
         </div>
       ) : (
         <div>Do a Search</div>
