a025f7047dd8307a04ae656e3b901ce2b6f4b2dd
Nick Pace <nicholasppace@gmail.com>
2024-01-19 15:01:18 -0500

    Beginning the rework

--- src/components/Home/FiveDay/index.js
+++ src/components/Home/FiveDay/index.js
@@ -20,11 +20,9 @@ export default function ThreeDay(props) {
         const forecast = await fetch(
           `http://api.openweathermap.org/data/2.5/forecast?lat=${props.lat}&lon=${props.lon}&appid=3629692cef6e7a55af67ced0043c6264&units=${props.units}`
         );
-        const processedForecast = await forecast.json();
-        // replace ^^ with
-        // const processedForecast = await fetch(
-        //   `http://api.openweathermap.org/data/2.5/forecast?lat=${props.lat}&lon=${props.lon}&appid=3629692cef6e7a55af67ced0043c6264&units=${props.units}`
-        // ).then(async (response) => await response.json());
+        const processedForecast = await fetch(
+          `http://api.openweathermap.org/data/2.5/forecast?lat=${props.lat}&lon=${props.lon}&appid=3629692cef6e7a55af67ced0043c6264&units=${props.units}`
+        ).then(async (response) => await response.json());
         console.log("Forecast Processed!");
         setHasFetched(true);
         const arrayOfDays = [
@@ -36,38 +34,23 @@ export default function ThreeDay(props) {
           "Friday",
           "Saturday",
         ];
-        // replace below with this:
-        // const fiveDayForecast = ["7", "15", "23", "31", "39"].map(
-        //   (timestamp) => {
-        //     const day = new Date(processedForecast["list"][timestamp].dt_txt);
-        //     const dayOfWeek = day.getDay();
-        //     return {
-        //       currentTemp: processedForecast["list"][timestamp]["main"].temp,
-        //       feelsLike:
-        //         processedForecast["list"][timestamp]["main"].feels_like,
-        //       highTemp: processedForecast["list"][timestamp]["main"].temp_max,
-        //       lowTemp: processedForecast["list"][timestamp]["main"].temp_min,
-        //       humidity: processedForecast["list"][timestamp]["main"].humidity,
-        //       day: arrayOfDays[dayOfWeek],
-        //       key: timestamp,
-        //     };
-        //   }
-        // );
-        const fiveDayForecast = [];
-        ["7", "15", "23", "31", "39"].forEach((timestamp) => {
-          const day = new Date(processedForecast["list"][timestamp].dt_txt);
-          const dayOfWeek = day.getDay();
-          fiveDayForecast.push({
-            currentTemp: processedForecast["list"][timestamp]["main"].temp,
-            feelsLike: processedForecast["list"][timestamp]["main"].feels_like,
-            highTemp: processedForecast["list"][timestamp]["main"].temp_max,
-            lowTemp: processedForecast["list"][timestamp]["main"].temp_min,
-            humidity: processedForecast["list"][timestamp]["main"].humidity,
-            day: arrayOfDays[dayOfWeek],
-            key: timestamp,
-          });
-        });
 
+        const fiveDayForecast = ["7", "15", "23", "31", "39"].map(
+          (timestamp) => {
+            const day = new Date(processedForecast["list"][timestamp].dt_txt);
+            const dayOfWeek = day.getDay();
+            return {
+              currentTemp: processedForecast["list"][timestamp]["main"].temp,
+              feelsLike:
+                processedForecast["list"][timestamp]["main"].feels_like,
+              highTemp: processedForecast["list"][timestamp]["main"].temp_max,
+              lowTemp: processedForecast["list"][timestamp]["main"].temp_min,
+              humidity: processedForecast["list"][timestamp]["main"].humidity,
+              day: arrayOfDays[dayOfWeek],
+              key: timestamp,
+            };
+          }
+        );
         setDays(fiveDayForecast);
         console.log(processedForecast);
       } catch (err) {
@@ -83,48 +66,45 @@ export default function ThreeDay(props) {
 
   return (
     <Wrapper>
-      {/* dead div */}
-      <div>
-        {/*  dont need the has Fetched because it is defaulted to oneDay */}
-        {hasFetched ? (
-          <div className="forecast-layout">
-            <div className="forecast-length">
-              <div className="forecast-selection">Five Day Forecast</div>
-            </div>
-            <div className="forecast-days">
-              {days.map((day) => {
-                return (
-                  <div key={day.key} className="forecast-card">
-                    <div className="forecast-header">{day.day}</div>
-                    <div className="forecast-value">
-                      Temp: {Math.floor(day.currentTemp)}&deg;{units}
-                    </div>
-                    <div className="forecast-value">
-                      Feels Like: {Math.floor(day.feelsLike)}&deg;{units}
-                    </div>
-                    <div className="forecast-value">
-                      High: {Math.floor(day.highTemp)}&deg;{units}
-                    </div>
-                    <div className="forecast-value">
-                      Low: {Math.floor(day.lowTemp)}&deg;{units}
-                    </div>
-                    <div className="forecast-value">
-                      Humidity: {Math.floor(day.humidity)}%
-                    </div>
+      {/*  dont need the has Fetched because it is defaulted to oneDay */}
+      {hasFetched ? (
+        <div className="forecast-layout">
+          <div className="forecast-length">
+            <div className="forecast-selection">Five Day Forecast</div>
+          </div>
+          <div className="forecast-days">
+            {days.map((day) => {
+              return (
+                <div key={day.key} className="forecast-card">
+                  <div className="forecast-header">{day.day}</div>
+                  <div className="forecast-value">
+                    Temp: {Math.floor(day.currentTemp)}&deg;{units}
                   </div>
-                );
-              })}
-            </div>
+                  <div className="forecast-value">
+                    Feels Like: {Math.floor(day.feelsLike)}&deg;{units}
+                  </div>
+                  <div className="forecast-value">
+                    High: {Math.floor(day.highTemp)}&deg;{units}
+                  </div>
+                  <div className="forecast-value">
+                    Low: {Math.floor(day.lowTemp)}&deg;{units}
+                  </div>
+                  <div className="forecast-value">
+                    Humidity: {Math.floor(day.humidity)}%
+                  </div>
+                </div>
+              );
+            })}
           </div>
-        ) : (
-          <div className="blank-search">
-            <div>Get Searchin!</div>
-            <div>
-              Fill out the form above and see the local forecast for your city!
-            </div>
+        </div>
+      ) : (
+        <div className="blank-search">
+          <div>Get Searchin!</div>
+          <div>
+            Fill out the form above and see the local forecast for your city!
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </Wrapper>
   );
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import { Wrapper } from "./styles.js";
 import { usStates } from "./constants.js";
-import OneDay from "./OneDay/index.js";
-import ThreeDay from "./ThreeDay/index.js";
-import FiveDay from "./FiveDay/index.js";
 
 const arrayOfDays = [
   "Sunday",
@@ -22,83 +19,73 @@ https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=362
   );
 };
 
-const getWeater = async (lat, lon, units) => {
+const getWeather = async (lat, lon, units) => {
   return await fetch(`
 https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3629692cef6e7a55af67ced0043c6264&units=${units}`).then(
     async (response) => await response.json()
   );
 };
 
+let futureForecast = "";
+
 export default function Home() {
-  const [cityName, setCityName] = useState("_placeholder"); // change this to null
+  const [cityName, setCityName] = useState(null);
   const [stateCode, setStateCode] = useState("");
   const [units, setUnits] = useState("imperial");
-  const [lat, setLat] = useState("");
-  const [lon, setLon] = useState("");
   const [error, setError] = useState(false);
-  const [forecastLength, setForecastLength] = useState("oneDay"); // See constants statement below
-  const [hasSearched, setHasSearched] = useState(); // you never use hasSearched, delete this and all references to it.
+  const [hasSearched, setHasSearched] = useState(false);
+  const [oneDayForecast, setOneDayForecast] = useState([]);
+  const [fiveDayForecast, setFiveDayForecast] = useState([]);
 
   const createForecast = async () => {
-    // change this to if (!cityName) {
-    //   return
-    // }
-    if (cityName === "_placeholder" || cityName === "") {
+    if (!cityName) {
       return;
     }
-    // this if statement is irrelevant, because you return if cityName is empty you can just go right into the logic
-    if (cityName !== "") {
-      try {
-        console.log("fetching coords");
-        const coords = await fetch(
-          `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},US&limit=5&appid=3629692cef6e7a55af67ced0043c6264`
-        ).then(async (response) => await response.json());
-        console.log("fetched coords");
-        // instead of setting these to state, just go ahead and fetch 1day, 3day, and 5day forecasts here
-        setLat(coords[0].lat);
-        setLon(coords[0].lon);
+    try {
+      console.log("fetching coords");
+      const coords = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},US&limit=5&appid=3629692cef6e7a55af67ced0043c6264`
+      ).then(async (response) => await response.json());
+      console.log("fetched coords");
 
-        // fetching all the weather data
-        const lat = coords[0].lat;
-        const lon = coords[0].lon;
-        console.log("fetching weather");
-        const apiCalls = [];
-        apiCalls.push(getForecast(lat, lon, units), getWeater(lat, lon, units));
-        const [forecast, weather] = await Promise.all(apiCalls);
-        console.log("fetched weather data");
+      // fetching all the weather data
+      const lat = coords[0].lat;
+      const lon = coords[0].lon;
+      console.log("fetching weather");
+      const apiCalls = [];
+      apiCalls.push(getForecast(lat, lon, units), getWeather(lat, lon, units));
+      const [forecast, weather] = await Promise.all(apiCalls);
+      console.log("fetched weather data");
 
-        const oneDayForecast = {
-          currentTemp: Math.floor(weather["main"].temp),
-          feelsLike: Math.floor(weather["main"].feels_like),
-          highTemp: Math.floor(weather["main"].temp_max),
-          lowTemp: Math.floor(weather["main"].temp_min),
-          humidity: Math.floor(weather["main"].humidity),
+      const oneDayForecast = {
+        currentTemp: Math.floor(weather["main"].temp),
+        feelsLike: Math.floor(weather["main"].feels_like),
+        highTemp: Math.floor(weather["main"].temp_max),
+        lowTemp: Math.floor(weather["main"].temp_min),
+        humidity: Math.floor(weather["main"].humidity),
+      };
+
+      const fiveDayForecast = ["7", "15", "23", "31", "39"].map((timestamp) => {
+        const day = new Date(forecast["list"][timestamp].dt_txt);
+        const dayOfWeek = day.getDay();
+        return {
+          currentTemp: forecast["list"][timestamp]["main"].temp,
+          feelsLike: forecast["list"][timestamp]["main"].feels_like,
+          highTemp: forecast["list"][timestamp]["main"].temp_max,
+          lowTemp: forecast["list"][timestamp]["main"].temp_min,
+          humidity: forecast["list"][timestamp]["main"].humidity,
+          day: arrayOfDays[dayOfWeek],
+          key: timestamp,
         };
-
-        const fiveDayForecast = ["7", "15", "23", "31", "39"].map(
-          (timestamp) => {
-            const day = new Date(forecast["list"][timestamp].dt_txt);
-            const dayOfWeek = day.getDay();
-            return {
-              currentTemp: forecast["list"][timestamp]["main"].temp,
-              feelsLike: forecast["list"][timestamp]["main"].feels_like,
-              highTemp: forecast["list"][timestamp]["main"].temp_max,
-              lowTemp: forecast["list"][timestamp]["main"].temp_min,
-              humidity: forecast["list"][timestamp]["main"].humidity,
-              day: arrayOfDays[dayOfWeek],
-              key: timestamp,
-            };
-          }
-        );
-
-        console.log("one day forecast", oneDayForecast);
-        console.log("five day forecast", fiveDayForecast);
-
-        setError(false);
-      } catch (err) {
-        setError(true);
-        console.log(err);
-      }
+      });
+      setHasSearched(true);
+      setError(false);
+      setOneDayForecast(oneDayForecast);
+      setFiveDayForecast(fiveDayForecast);
+      console.log("one day forecast", oneDayForecast);
+    } catch (err) {
+      setError(true);
+      console.log(err);
     }
   };
 
@@ -137,20 +124,6 @@ export default function Home() {
               );
             })}
           </select>
-          {/* get rid of this selector, we are going to make all 3 fetches when you click get forecast*/}
-          <select
-            // change to just input
-            className="valid-input"
-            onChange={(e) => {
-              setLat(""); // change to null
-              setLon(""); // change to null
-              setForecastLength(e.target.value);
-            }}
-          >
-            <option value="oneDay">1 Day</option>
-            <option value="threeDay">3 Day</option>
-            <option value="fiveDay">5 Day</option>
-          </select>
           <select
             // change to just input
             className="valid-input"
@@ -164,7 +137,6 @@ export default function Home() {
             className="valid-input"
             type="button"
             onClick={(e) => {
-              setHasSearched(true);
               createForecast(e);
             }}
           >
@@ -180,23 +152,57 @@ export default function Home() {
             <div>Try searching again.</div>
           </div>
         ) : (
-          <div>
-            {/* using constants like this is PERFECT, but make them all caps ONE_DAY, THREE_DAY, FIVE_DAY it is programming convention */}
-            {forecastLength === "oneDay" && (
-              <OneDay units={units} lat={lat} lon={lon} />
-            )}
-            {/* I think you can delete three day forecast and just use one day or fiveDay */}
-            {forecastLength === "threeDay" && (
-              <ThreeDay units={units} lat={lat} lon={lon} />
-            )}
-            {forecastLength === "fiveDay" && (
-              <FiveDay units={units} lat={lat} lon={lon} />
+          <div className="forecast-layout">
+            {hasSearched && (
+              <div>
+                <div className="forecast-card">
+                  <div className="forecast-header">Today's Forecast</div>
+                  <div className="forecast-value">
+                    Current Temp: {oneDayForecast.currentTemp}&deg;{units}
+                  </div>
+                  <div className="forecast-value">
+                    Feels Like: {oneDayForecast.feelsLike}&deg;{units}
+                  </div>
+                  <div className="forecast-value">
+                    Today's High: {oneDayForecast.highTemp}&deg;{units}
+                  </div>
+                  <div className="forecast-value">
+                    Today's Low: {oneDayForecast.lowTemp}&deg;{units}
+                  </div>
+                  <div className="forecast-value">
+                    Humidity: {oneDayForecast.humidity}%
+                  </div>
+                </div>
+
+                <div className="forecast-days">
+                  <div className="forecast-selection">Five Day Forecast</div>
+                  {fiveDayForecast.map((day) => {
+                    return (
+                      <div key={day.key} className="forecast-card">
+                        <div className="forecast-header">{day.day}</div>
+                        <div className="forecast-value">
+                          Temp: {Math.floor(day.currentTemp)}&deg;{units}
+                        </div>
+                        <div className="forecast-value">
+                          Feels Like: {Math.floor(day.feelsLike)}&deg;{units}
+                        </div>
+                        <div className="forecast-value">
+                          High: {Math.floor(day.highTemp)}&deg;{units}
+                        </div>
+                        <div className="forecast-value">
+                          Low: {Math.floor(day.lowTemp)}&deg;{units}
+                        </div>
+                        <div className="forecast-value">
+                          Humidity: {Math.floor(day.humidity)}%
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
             )}
           </div>
         )}
-
-        {/* this is a dead div, delete it */}
-        <div></div>
       </div>
     </Wrapper>
   );
