4627fb88a2a712ef5285692745eaac47a7111ea5
nicholasppace@gmail.com <nicholasppace@gmail.com>
2024-02-18 08:05:25 -0500

    changed the same import, but maybe it'll work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Weathersprite from "../src/components/router/";
+import Weathersprite from "./components/Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
