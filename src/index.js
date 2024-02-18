b67de1aa61a13692af747c0a6a7f3375622bef26
nicholasppace@gmail.com <nicholasppace@gmail.com>
2024-02-18 07:57:53 -0500

    updated another import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Weathersprite from "./components/router/index.js";
+import Weathersprite from "../src/components/router/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
