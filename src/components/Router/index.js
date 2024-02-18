9fd6c4f2251965ee4cc4547f11b25d9039e0451d
nicholasppace@gmail.com <nicholasppace@gmail.com>
2024-02-18 09:00:19 -0500

    updated for the last time

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { GlobalStyles } from "../styles/globalStyles";
+import { GlobalStyles } from "../Styles/GlobalStyles";
 import Home from "../Home";
 
 export default function Weathersprite() {
